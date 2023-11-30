2f3d61b2fdba4d44c5b207c060e224dceef87208
MercyNJ <mercinjuguna@gmail.com>
2023-11-30 10:01:00 +0000

    A pricing clas

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,41 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be an instance of Currency class');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency._name} (${this.currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('ConversionRate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
