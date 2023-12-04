85a5d4627360ff12ebd111a326b536eb6f02cf03
MercyNJ <mercinjuguna@gmail.com>
2023-12-04 11:48:08 +0000

    map

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(groceriesMap) {
+  if (!(groceriesMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [key, value] of groceriesMap) {
+    if (value === 1) {
+      groceriesMap.set(key, 100);
+    }
+  }
+}
