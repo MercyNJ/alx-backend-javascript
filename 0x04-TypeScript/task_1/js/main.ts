8244101480f9eb28f6d45c275d7180777d2bec32
MercyNJ <mercinjuguna@gmail.com>
2023-12-06 10:18:17 +0000

    Working with interfaces

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,52 @@
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key:string]: any;
+  }
+export interface Directors extends Teacher {
+  numberOfReports: number;
+  }
+
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+  }
+
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string) => {
+  return `${firstName[0]}. ${lastName}`;
+  }
+
+export interface IStudent {
+  firstName: string;
+  lastName: string;
+}
+
+export interface IStudentClass extends IStudent {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements IStudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+  }
+
+const student = new StudentClass("Mercy", "NJ");
+
+console.log(student.displayName());
+console.log(student.workOnHomework());
