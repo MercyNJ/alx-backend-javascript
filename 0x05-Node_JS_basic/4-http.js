5e2f5f8f7e1d6d72f84df8f1bc1e68f37063d954
MercyNJ <mercinjuguna@gmail.com>
2024-01-30 09:11:10 +0000

    Create a small HTTP server using Node HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,14 @@
 const http = require('http');
 
+const PORT = 1245;
+const host = '127.0.0.1';
+
 const app = http.createServer((req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!\n');
 });
 
-const PORT = 1245;
-const host = '127.0.0.1';
 app.listen(PORT, host, () => {
 });
 
