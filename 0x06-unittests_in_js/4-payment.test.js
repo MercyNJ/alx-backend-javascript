18e4c44c732b07d3159d7c626753eaa1d8b0c64d
MercyNJ <mercinjuguna@gmail.com>
2024-02-01 12:19:25 +0000

    using sinon to create stub

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,20 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function () {
+  it('validates the usage of Utils.calculateNumber with sinon.stub', () => {
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleLogSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+});
