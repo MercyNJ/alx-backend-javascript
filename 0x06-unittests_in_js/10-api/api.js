742e0f212932e35a0b47d19b169c03ac37b856c5
MercyNJ <mercinjuguna@gmail.com>
2024-02-01 15:32:31 +0000

    Deep equality & Post integration testing

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  const cartId = req.params.id;
+  res.send(`Payment methods for cart ${cartId}`);
+});
+
+app.get('/available_payments', (req, res) => {
+  const paymentMethods = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  };
+  res.json(paymentMethods);
+});
+
+app.post('/login', (req, res) => {
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
+  }
+
+  res.send(`Welcome ${username}`);
+});
+
+module.exports = app;
