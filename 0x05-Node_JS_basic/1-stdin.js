ba37ab48caaabc2c4ad8d1d9cd016394f9e67a75
MercyNJ <mercinjuguna@gmail.com>
2024-02-02 08:53:55 +0000

    correction on stubs

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,14 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
+process.stdin.setEncoding('utf8');
+
+process.stdin.once('data', (chunk) => {
+  const name = chunk.trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+
+  if (!process.stdin.isTTY) {
     process.stdout.write('This important software is now closing\n');
-  });
-}
+  }
+
+  process.exit();
+});
